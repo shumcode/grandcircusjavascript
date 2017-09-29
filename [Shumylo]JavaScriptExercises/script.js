1b66a486427abf3ed5b4b8a6581b17ab90021103
shumcode <cshumylo@gmail.com>
2017-09-28 20:26:03 -0400

    haha commands

--- [Shumylo]JavaScriptExercises/script.js
+++ [Shumylo]JavaScriptExercises/script.js
@@ -1,7 +1,23 @@
+//For loop adding all numbers leading to sum
+
 var input = prompt("Enter any number!");
 var sum = 0;
 
 for (var i = 1; i <= input; i++) {
   sum += i;
 }
- console.log(sum);
+console.log("The total of all numbers leading to your input: " +sum);
+
+//Do...While loop (Would you like to play a game?)
+
+var prompt = prompt("Would you like to play a game?");
+
+// if (prompt = 'yes') {
+//   prompt("Enter a WORD")
+// }
+
+function openPrompt() {
+  if (prompt = 'yes') {
+    prompt[i]("Enter a word.");
+  }
+}
